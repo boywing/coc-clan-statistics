880f9d04cf455e8e5f20bef9ea776da11c8a2154
Mattias Sörlin <ranjorclash@gmail.com>
2019-09-12 22:06:34 +0200

    Added www_path to config.php

--- config.php
+++ config.php
@@ -3,6 +3,7 @@
 # Global variables
 $install_path = "/srv/www/htdocs/stats/";           #Path to install folder
 $update_path = "/srv/www/htdocs/stats/update/";     #Path to update folder
+$www_path = '"/statistik/"';                        #WWW path to index.php
 
 $days = 60;                                 # Amount of days to show in statistics.
 $site_title = "Aktiva Vikingars Statistik"; # The sites title text
